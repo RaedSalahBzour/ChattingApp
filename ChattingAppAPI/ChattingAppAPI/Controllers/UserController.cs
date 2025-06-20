ea291abc046873c85cb2015774e782a872110ed8
Raed Salah Bzour <raedbzour77@gmail.com>
2025-06-20 03:16:29 +0300

    Fix: Convert API and Client folders from submodules to normal folders

---
+++ ChattingAppAPI/ChattingAppAPI/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using ChattingAppAPI.Data;
+using ChattingAppAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ChattingAppAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return Ok(await context.Users.ToListAsync());
+    }
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    {
+        AppUser? user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        if (user is null)
+            return NotFound();
+        return Ok(user);
+    }
+}
---
+++ ChattingAppAPI/ChattingAppAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ChattingAppAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingAppAPI.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+
+}
+
+
+
+
+
