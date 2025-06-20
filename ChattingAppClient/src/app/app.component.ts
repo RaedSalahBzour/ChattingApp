9445a6107bf5482e850e4e99e8a94d67ca7be4c6
Raed Salah Bzour <raedbzour77@gmail.com>
2025-06-20 05:18:50 +0300

    adding get and cors

--- ChattingAppClient/src/app/app.component.ts
+++ ChattingAppClient/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ChattingAppClient';
+export class AppComponent implements OnInit {
+  users: any;
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/user').subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => console.log(error),
+      complete: () => {
+        console.log('request has completed');
+      },
+    });
+  }
 }
