0e4fd03c6f2fc7c96886d4e4f087bc1da689d903
Raed Salah Bzour <raedbzour77@gmail.com>
2025-06-20 05:20:40 +0300

    adding cors

--- ChattingAppAPI/ChattingAppAPI/Program.cs
+++ ChattingAppAPI/ChattingAppAPI/Program.cs
@@ -10,10 +10,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.MapControllers();
